9572ae776f406eb4dff79f088adfff365cb12469
PatrykWski <patrykwiszniewski96@gmail.com>
2026-03-22 16:20:12 +0100

    last

--- src/main/java/core/basesyntax/repository/StorageInterface.java
+++ src/main/java/core/basesyntax/repository/StorageInterface.java
@@ -3,6 +3,7 @@ package core.basesyntax.repository;
 import java.util.Map;
 
 public interface StorageInterface {
+
     Map<String, Integer> getAll();
     void put(String fruitName, int quantity);
     int getQuantity(String fruitName);
